1ab62be6279fa30a0317f0d264e9a123c45e8f91
Michael Fisher <fisherdesign3@gmail.com>
2022-08-11 19:50:13 -0600

    Main logic for user selection is done. Need to build out what happens at each option now

--- index.js
+++ index.js
@@ -6,27 +6,95 @@ startPrompt = () => {
 			{
 				message: 'Please Select an option from below',
 				type: 'list',
+				name: 'reply',
 				choices: [
-					'View All Departments',
-					'View All Roles',
-					'View All Employees',
-					'Add A Department',
-					'Add A Role',
-					'Add An Employee',
-					'Update An Employee Role',
-					'Exit',
+					{
+						name: 'View All Departments',
+						value: 'viewAllDepartments',
+					},
+					{
+						name: 'View All Roles',
+						value: 'viewAllRoles',
+					},
+					{
+						name: 'View All Employees',
+						value: 'viewAllEmployees',
+					},
+					{
+						name: 'Add A Department',
+						value: 'addADepartment',
+					},
+					{
+						name: 'Add A Role',
+						value: 'addARole',
+					},
+					{
+						name: 'Add An Employee',
+						value: 'addAnEmployee',
+					},
+					{
+						name: 'Update An Employee Role',
+						value: 'updateAnEmployeeRole',
+					},
+					{
+						name: 'Exit',
+						value: 'exit',
+					},
 				],
-				name: 'prompt',
 			},
 		])
 		.then((answers) => {
-			if (answers.prompt === 'Exit') {
+			if (answers.reply === 'exit') {
 				console.log('Goodbye!');
 			} else {
-				console.log(answers);
-				startPrompt();
+				console.log(answers.reply);
+				userChoice(answers.reply);
 			}
 		});
 };
 
+const viewAllDepartments = () => {
+	console.log('You have selected to view all departments');
+};
+
+const viewAllRoles = () => {
+	console.log('You have selected to view all roles.');
+};
+
+const viewAllEmployees = () => {
+	console.log('You have selected to view all employees');
+};
+
+const addADepartment = () => {
+	console.log('You have selected to add a department');
+};
+
+const addARole = () => {
+	console.log('You have selected to add a role');
+};
+
+const addAnEmployee = () => {
+	console.log('You have selected to add an employee');
+};
+
+const updatedAnEmployeeRole = () => {
+	console.log('You have selected to update an employee role');
+};
+
+const functionList = {
+	viewAllDepartments,
+	viewAllRoles,
+	viewAllEmployees,
+	addADepartment,
+	addARole,
+	addAnEmployee,
+	updatedAnEmployeeRole,
+};
+
+const userChoice = (answer) => {
+	functionList[answer]();
+	startPrompt();
+};
+
+//Initalize the Script
 startPrompt();
